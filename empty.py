aaf52471d07addc556020d6fd621f8195262e6ac
kiran reddy kancharla <kiran.kancharla92@gmail.com>
2024-10-26 10:13:14 -0500

    Update empty.py

--- empty.py
+++ empty.py
@@ -1,19 +1,6 @@
-"""
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from app import create_app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-db = SQLAlchemy(app)
+app = create_app()
 
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return "<h1>Hello, World!</h1><p>Welcome to my dynamic website.</p>"
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-"""
